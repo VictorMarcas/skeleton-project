6589d20694db230d7cfe3e2c8244806fb11cae56
Victor Marcas <victor11687@gmail.com>
2022-04-03 03:01:31 -0500

    template project webpack-vue-tailwindcss

---
+++ src/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex items-center justify-center w-full h-screen bg-gray-300">
+        <div class="text-xl font-bold text-white uppercase">{{ message }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {
+                message: 'Hola Mundo'
+            }
+        }
+    }
+</script>
